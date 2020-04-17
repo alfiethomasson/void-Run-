bf9828831cc0701d45354eda28265e85a47102a6
CiaranAndrewBrady <40312302@live.napier.ac.uk>
2020-04-17 02:45:55 +0100

    Default attack structures for medium and tough enemies
    TODO: Add unique abiliies for different enemy types to increase variation

--- void_run/enemy_tough.cpp
+++ void_run/enemy_tough.cpp
@@ -10,14 +10,19 @@ void ToughEnemy::update(double dt)
 	if (isTurn && isFinishedTurn != true)
 	{
 		srand(time(0));
-		int enemyAI = rand() % 2; //Random number from 0-1. 0 is attack, 1 is pass.
-		if (enemyAI == 0) {
-			std::cout << "The enemy attacks!";
-			attackEnemy(_strength);
+		int enemyAI = rand() % 6; //Random number from 0-5. 1-2 is medium attack, 3-4 is unique ability, 5 is a heavy attack.
+		if (enemyAI == 0 || enemyAI == 1 || enemyAI == 2) {
+			std::cout << "The enemy makes a weak attack! \n";
+			attackEnemy(_strength+5);
 			EndTurn();
 		}
-		else if (enemyAI == 1) {
-			std::cout << "The enemy passes its turn!";
+		else if (enemyAI == 3 || enemyAI == 4) {
+			std::cout << "The enemy would use its unique attack here, if I'd programmed it! \n";
+			EndTurn();
+		}
+		else if (enemyAI == 5) {
+			std::cout << "The enemy makes a strong attack! \n";
+			attackEnemy(_strength*2);
 			EndTurn();
 		}
 	}
