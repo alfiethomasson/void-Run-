b1ba35b48fedf27942aae81cf9170f0788e9fe5b
CiaranAndrewBrady <40312302@live.napier.ac.uk>
2020-04-21 20:15:20 +0100

    Added buttons and junk

--- void_run/LevelUpRoom.cpp
+++ void_run/LevelUpRoom.cpp
@@ -6,6 +6,8 @@
 #include "cmp_sprite.h"
 #include "VoidRun.h"
 
+bool choicePicked;
+
 LevelUpRoom::LevelUpRoom(std::shared_ptr <Entity> p)
 	: Room(p) {};
 
@@ -14,23 +16,65 @@ void LevelUpRoom::Load() {
 	auto a = player->GetCompatibleComponent<BasePlayerComponent>();
 	p = a[0];
 
+	choicePicked = false;
+
+	statIncrease1.setTexture(statUPTex);
+	statIncrease2.setTexture(statUPTex);
+	statIncrease3.setTexture(statUPTex);
+	statIncrease1.setScale(0.3f, 0.3f);
+	statIncrease2.setScale(0.3f, 0.3f);
+	statIncrease3.setScale(0.3f, 0.3f);
+	statIncrease1.setRotation(90);
+	statIncrease2.setRotation(90);
+	statIncrease3.setRotation(90);
+	statIncrease1.setPosition(500.0f, 250.0f);
+	statIncrease2.setPosition(700.0f, 250.0f);
+	statIncrease3.setPosition(900.0f, 250.0f);
+
+	statIncrease1Box = statIncrease1.getGlobalBounds();
+	statIncrease2Box = statIncrease2.getGlobalBounds();
+	statIncrease3Box = statIncrease3.getGlobalBounds();
 }	
 
 void LevelUpRoom::Update(const double& dt) {
 
+	if (!statUPTex.loadFromFile("res/Icons/Arrow.png"))
+	{
+		std::cout << "Couldnt load up arrow in levelling up room\n";
+	}
+
+	sf::Vector2i tempPos = sf::Mouse::getPosition(Engine::GetWindow());
+	sf::Vector2f cursPos = sf::Vector2f(tempPos);
+
+	/*if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+	{
+		if (statIncrease1Box.contains(cursPos))
+		{
+			p->addStats(5, 0, 0);
+			choicePicked = true;
+		}
+		if (statIncrease2Box.contains(cursPos))
+		{
+			p->addStats(0, 10, 0);
+			choicePicked = true;
+		}
+		if (statIncrease3Box.contains(cursPos))
+		{
+			p->addStats(0, 0, 5);
+			choicePicked = true;
+		}
+	}*/
 }
 
 void LevelUpRoom::Render() {
-	for (auto& e : cells)
-	{
-		Renderer::queue(&e);
-	}
-
-	if (!choicePicked)
-	{
-		Renderer::queue(&ability1);
-		Renderer::queue(&ability2);
-		Renderer::queue(&ability1Text);
-		Renderer::queue(&ability2Text);
-	}
+	//Renderer::queue(&ability1);
+	//Renderer::queue(&ability2);
+	Renderer::queue(&statIncrease1);
+	Renderer::queue(&statIncrease2);
+	Renderer::queue(&statIncrease3);
+	//Renderer::queue(&ability1Text);
+	//Renderer::queue(&ability2Text);
+	Renderer::queue(&statIncrease1Text);
+	Renderer::queue(&statIncrease2Text);
+	Renderer::queue(&statIncrease3Text);
 }
--- void_run/LevelUpRoom.h
+++ void_run/LevelUpRoom.h
@@ -2,22 +2,35 @@
 #include <iostream>
 #include "Room.h"
 #include "cmp_player.h"
+#include "engine.h"
 
 class LevelUpRoom : public Room {
 protected:
 	sf::Font font;
-	sf::Text playerHP;
-	sf::Text experienceCounter;
-
 	sf::Texture statUPTex;
+
+	//sf::Text playerHP;
+	//sf::Text experienceCounter;
+
+	std::vector<sf::Sprite> cells;
+
 	sf::Sprite ability1;
 	sf::Sprite ability2;
+	sf::Sprite statIncrease1;
+	sf::Sprite statIncrease2;
+	sf::Sprite statIncrease3;
 
-	sf::Text RewardsText;
-	sf::Text StrengthText;
-	sf::Text HealthText;
+	sf::Text ability1Text;
+	sf::Text ability2Text;
+	sf::Text statIncrease1Text;
+	sf::Text statIncrease2Text;
+	sf::Text statIncrease3Text;
+
 	sf::FloatRect ability1Box;
 	sf::FloatRect ability2Box;
+	sf::FloatRect statIncrease1Box;
+	sf::FloatRect statIncrease2Box;
+	sf::FloatRect statIncrease3Box;
 public:
 
 	LevelUpRoom(std::shared_ptr<Entity> p);
