39c5db8190350e3fb0586698a5bd88dceb660e0a
Alfie Thomasson <alfiethomasson0@gmail.com>
2020-04-26 22:10:13 +0100

    delete override

--- void_run/VoidRun.h
+++ void_run/VoidRun.h
@@ -69,7 +69,6 @@ public:
 	void Update(const double& dt) override;
 	void Render() override;
 	void Load() override;
-	void UnLoad() override;
 	void ChangeControls();
 	void UpdateButtons();
 	void setSettings(bool tf);
